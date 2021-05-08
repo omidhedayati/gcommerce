13de8fb3e45b6278c811dbfd961a4272a8df66a4
omidhedayati <59288262+omidhedayati@users.noreply.github.com>
2021-05-08 15:56:52 +0430

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -46,3 +46,10 @@ module.exports = withCommerceConfig({
 
 // Don't delete this console log, useful to see the commerce config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+const withImages = require('next-images')
+module.exports = withImages({
+  fileExtensions: ["jpg", "jpeg", "png", "gif"],
+  webpack(config, options) {
+    return config
+  }
+})
